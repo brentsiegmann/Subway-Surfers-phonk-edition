2882d9793711582f2ba29cdad5a6cd4bead34b41
brentsiegmann <brentsiegmann@gmail.com>
2025-08-09 23:27:15 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,9 +3,24 @@ import ReactDOM from "react-dom/client";
 
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-      <p>Hallo wereld</p>
+    <div className="app">
+      <Floor />
+    </div>
+  );
+}
+
+function Floor() {
+  return (
+    <div className="floor">
+      <div className="tile1 tile"></div>
+      <div className="tile2 tile"></div>
+      <div className="tile3 tile"></div>
+      <div className="tile4 tile"></div>
+      <div className="tile5 tile"></div>
+      <div className="tile6 tile"></div>
+      <div className="tile7 tile"></div>
+      <div className="tile8 tile"></div>
+      <div className="tile9 tile"></div>
     </div>
   );
 }
