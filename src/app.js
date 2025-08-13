c074126c8936f31f051c1852fe618a2c430c9299
brentsiegmann <brentsiegmann@gmail.com>
2025-08-13 22:50:44 +0200

    veelBeter

--- src/app.js
+++ src/app.js
@@ -1,31 +1,20 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 export default function App() {
-  const tiles = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
-  const [aan, setAan] = useState(
-    tiles.map((tile) => {
-      if (tile === "b2") return { id: tile, status: true };
-      return { id: tile, status: false };
-    })
-  );
+  const [pos, setPos] = useState({ row: 1, col: 1 });
 
-  function handleTileUpdate(newPlace) {
-    setAan(
-      tiles.map((tile) => {
-        if (tile === newPlace) return { id: tile, status: true };
-        return { id: tile, status: false };
-      })
-    );
+  function newPos(obj) {
+    setPos(obj);
   }
 
   return (
     <div className="app">
-      <Floor aan={aan} tiles={tiles} />
-      <Movement aan={aan} onTileUpdate={handleTileUpdate} />
+      <Floor pos={pos} />
+      <Movement pos={pos} newPos={newPos} />
     </div>
   );
 }
 
-function Movement({ aan, onTileUpdate }) {
+function Movement({ pos, newPos }) {
   useEffect(() => {
     const handleKeyDown = (event) => {
       if (
@@ -34,54 +23,23 @@ function Movement({ aan, onTileUpdate }) {
         event.key === "ArrowLeft" ||
         event.key === "ArrowRight"
       ) {
-        let previousPlace;
-        aan.forEach((tile) => {
-          if (tile.status === true) {
-            previousPlace = tile.id;
-          }
-        });
-        let newPlace;
+        let { row, col } = pos;
+        console.log(event.key);
 
         if (event.key === "ArrowUp") {
-          if (previousPlace[0] === "c") {
-            newPlace = "b" + previousPlace[1];
-          }
-          if (previousPlace[0] === "b") {
-            newPlace = "a" + previousPlace[1];
-          }
+          if (col > 0) col -= 1;
         }
-
         if (event.key === "ArrowDown") {
-          if (previousPlace[0] === "b") {
-            newPlace = "c" + previousPlace[1];
-          }
-          if (previousPlace[0] === "a") {
-            newPlace = "b" + previousPlace[1];
-          }
+          if (col < 2) col += 1;
         }
-
         if (event.key === "ArrowLeft") {
-          if (previousPlace[1] === "2") {
-            newPlace = previousPlace[0] + "1";
-          }
-          if (previousPlace[1] === "3") {
-            newPlace = previousPlace[0] + "2";
-          }
+          if (row > 0) row -= 1;
         }
-
         if (event.key === "ArrowRight") {
-          if (previousPlace[1] === "2") {
-            newPlace = previousPlace[0] + "3";
-          }
-          if (previousPlace[1] === "1") {
-            newPlace = previousPlace[0] + "2";
-          }
+          if (row < 2) row += 1;
         }
 
-        console.log("newplace:", newPlace);
-        if (newPlace) {
-          onTileUpdate(newPlace);
-        }
+        newPos({ row, col });
       }
     };
 
@@ -89,14 +47,21 @@ function Movement({ aan, onTileUpdate }) {
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [onTileUpdate]);
+  });
 }
-
-function Floor({ aan }) {
+function Floor({ pos }) {
   return (
     <div className="floor">
-      {aan.map((tile) => (
-        <div className="tile" key={tile.id} style={tile.status ? { backgroundColor: "red" } : {}}></div>
+      {[0, 1, 2].map((row) => (
+        <div className="row" key={row}>
+          {[0, 1, 2].map((col) => (
+            <div
+              className="tile"
+              key={`${col}`}
+              style={pos.row === row && pos.col === col ? { backgroundColor: "red" } : {}}
+            ></div>
+          ))}
+        </div>
       ))}
     </div>
   );
